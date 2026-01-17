4ffb829a07282906271254dc106e9de21b15bed1
Tekknobot <thrillatron@hotmail.com>
2026-01-17 04:44:39 -0500

    Night push.

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -5,11 +5,18 @@ extends Camera2D
 
 @onready var terrain := $"../Terrain"
 
-func _ready():
+# --- Middle-mouse drag pan ---
+@export var drag_button: MouseButton = MOUSE_BUTTON_MIDDLE
+@export var drag_sensitivity := 1.0
+
+var _dragging := false
+var _last_mouse_pos := Vector2.ZERO
+
+func _ready() -> void:
 	await get_tree().process_frame
 	center_on_map()
 
-func center_on_map():
+func center_on_map() -> void:
 	# Get the world position of top-left and bottom-right tiles
 	var top_left = terrain.map_to_local(Vector2i(0, 0))
 	var bottom_right = terrain.map_to_local(Vector2i(map_width, map_height))
@@ -20,5 +27,24 @@ func center_on_map():
 
 	# Center point
 	var center = (top_left + bottom_right) * 0.5
-
 	global_position = center
+
+func _input(event: InputEvent) -> void:
+	# Start/stop dragging with mouse wheel button (middle mouse)
+	if event is InputEventMouseButton and event.button_index == drag_button:
+		if event.pressed:
+			_dragging = true
+			_last_mouse_pos = event.position
+		else:
+			_dragging = false
+		get_viewport().set_input_as_handled()
+
+	# While dragging, move camera opposite to mouse movement
+	if _dragging and event is InputEventMouseMotion:
+		var delta = event.position - _last_mouse_pos
+		_last_mouse_pos = event.position
+
+		# Dragging should "grab" the world: move camera opposite the mouse
+		global_position -= delta * drag_sensitivity / zoom
+
+		get_viewport().set_input_as_handled()
